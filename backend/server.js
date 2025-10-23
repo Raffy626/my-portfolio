95628cb83164f40c2bbddb311ebad69ce2958f7c
Raffy626 <reshainapasha@gmail.com>
2025-10-23 13:43:15 +0700

    remove last port

--- backend/server.js
+++ backend/server.js
@@ -21,14 +21,19 @@ app.get("/api", (req, res) => {
   res.json({ message: "API is working!" });
 });
 
-const PORT = process.env.PORT || 5000;
 
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("✅ MongoDB connected");
-    app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
   })
   .catch((err) => {
     console.error("❌ MongoDB connection error:", err.message);
   });
+
+if (process.env.NODE_ENV !== 'production') {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+}
+
+export default app;
